9cb7183a5477d4d1d62d1de861ec1887f561a31e
AdaLollA <lorenz.graaf@gmail.com>
2025-09-27 23:11:01 +0200

    image init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="h-[5000]">
+    <main className="h-[5000] pt-28 sm:pt-36">
       <Header />
+      <Intro />
     </main>
   );
 }
