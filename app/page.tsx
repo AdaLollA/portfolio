41cd862631ef65c3cb1664ca326481a3f6b99484
AdaLollA <lorenz.graaf@gmail.com>
2025-09-27 23:29:54 +0200

    intro text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="h-[5000] pt-28 sm:pt-36">
+    <main className="h-[5000] pt-28 sm:pt-36 flex flex-col items-center px-4">
       <Header />
       <Intro />
     </main>
--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Intro() {
     return (
-        <section>
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
@@ -30,13 +30,16 @@ export default function Intro() {
                     </motion.span>
                 </div>
             </div>
-            <p>
+            <motion.p
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
                 <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
                 <span className="font-bold">full-stack developer</span> with{" "}
                 <span className="font-bold">8 years</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
                 <span className="underline">React (Next.js)</span>.
-            </p>
+            </motion.p>
         </section>
     )
 }
