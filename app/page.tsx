af8ee34594cbcab5c4e47d0db286461f13585777
AdaLollA <lorenz.graaf@gmail.com>
2025-09-28 15:16:17 +0200

    divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header";
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="h-[5000] pt-28 sm:pt-36 flex flex-col items-center px-4">
       <Header />
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
