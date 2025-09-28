15dfe4ad88dbbf860e902d291c7b628f31f9b4c7
AdaLollA <lorenz.graaf@gmail.com>
2025-09-28 16:38:13 +0200

    project card animation

--- components/projects.tsx
+++ components/projects.tsx
@@ -36,7 +36,16 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
             </ul>
         </div>
         <Image
-            className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40'
+            className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 
+            
+            group-hover:-translate-x-3 
+            group-hover:translate-y-3 
+            group-hover:-rotate-2  
+
+            group-hover:scale-[1.04] transition 
+
+            group-even:group-hover:translate-x-3 
+            group-even:group-hover:rotate-2'
             src={imageUrl}
             alt={title}
             quality={95} />
