4d059ea9766d27eb3a4a03180cf07c22c1298790
AdaLollA <lorenz.graaf@gmail.com>
2025-09-29 22:14:06 +0200

    email works

--- actions/sendEmail.tsx
+++ actions/sendEmail.tsx
@@ -3,19 +3,22 @@
 import { validateString } from '@/lib/utils';
 import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function sendEmail(formData: FormData) {
-  const resend = new Resend(process.env.RESEND_API_KEY);
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
+  const recipientEmail = process.env.RESEND_RECEIVER;
 
   // simple server-side validation
   if (!validateString(senderEmail, 500)) { return { error: 'invalid senderEmail' } }
   if (!validateString(message, 5000)) { return { error: 'invalid message' } }
+  if (!validateString(recipientEmail, 500)) { return { error: 'invalid recipient email' } }
 
   try {
     const { data } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['portfolio.void989@passmail.com'],
+      to: recipientEmail as string,
       subject: 'Portfolio Contact-Request',
       replyTo: senderEmail as string,
       html: message as string
@@ -25,5 +28,5 @@ export async function sendEmail(formData: FormData) {
   } catch (error) {
     console.log(error);
   }
-  return { error: 'error while sending email'}
+  return { error: 'error while sending email' }
 }
