c2dd9fd07c70ba99211a5f8e1dc2148255c2214a
AdaLollA <lorenz.graaf@gmail.com>
2025-09-29 20:53:34 +0200

    frontend validation

--- components/contact.tsx
+++ components/contact.tsx
@@ -21,11 +21,19 @@ export default function Contact() {
 
             <form className="mt-10 flex flex-col">
                 <input
+                    required
+                    maxLength={500}
                     type="email"
                     className="h-14 rounded-lg border borderBlack px-4"
                     placeholder="Your email" />
-                <textarea className="h-52 my-3 rounded-lg borderBlack px-4 py-4" placeholder="Your message" />
-                <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer hover:bg-gray-950">
+                <textarea
+                    required
+                    maxLength={500}
+                    className="h-52 my-3 rounded-lg borderBlack px-4 py-4"
+                    placeholder="Your message" />
+                <button
+                    type="submit"
+                    className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer hover:bg-gray-950">
                     Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
                 </button>
             </form>
