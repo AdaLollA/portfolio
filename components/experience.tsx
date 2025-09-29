6065c4b3e04775238d15b69d9fcb2d1ef1ebc46d
AdaLollA <lorenz.graaf@gmail.com>
2025-09-29 20:20:13 +0200

    scroll bugfix

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,7 +9,8 @@ import { experiencesData } from '@/lib/data';
 export default function Experience() {
     return (
         <section
-            id='experience'>
+            id='experience'
+            className='scroll-mt-28 mb-28 sm:mb-40'>
             <SectionHeading>My experience</SectionHeading>
             <VerticalTimeline lineColor=''>
                 {
