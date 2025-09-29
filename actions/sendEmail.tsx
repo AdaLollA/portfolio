59623b20046e936c0a462001423772ac8095930d
AdaLollA <lorenz.graaf@gmail.com>
2025-09-29 22:02:27 +0200

    debugging email

--- actions/sendEmail.tsx
+++ actions/sendEmail.tsx
@@ -4,25 +4,26 @@ import { validateString } from '@/lib/utils';
 import { Resend } from 'resend';
 
 export async function sendEmail(formData: FormData) {
-  console.log("running on server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
-
   const resend = new Resend(process.env.RESEND_API_KEY);
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
   // simple server-side validation
   if (!validateString(senderEmail, 500)) { return { error: 'invalid senderEmail' } }
-  if (!validateString(senderEmail, 5000)) { return { error: 'invalid message' } }
+  if (!validateString(message, 5000)) { return { error: 'invalid message' } }
 
-  const { data } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
-    to: ['portfolio.void989@passmail.com'],
-    subject: 'Portfolio Kontaktformular-Anfrage',
-    replyTo: senderEmail as string,
-    html: message as string
-  });
-
-  console.log(data);
+  try {
+    const { data } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['portfolio.void989@passmail.com'],
+      subject: 'Portfolio Kontaktformular-Anfrage',
+      replyTo: senderEmail as string,
+      html: message as string
+    });
+    console.log('resend response', data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+  return { error: 'error while sending email'}
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,16 @@
 export const validateString = (value: unknown, maxLength: number) => {
-  if (!value || typeof value !== "string" || value.length > maxLength) {
-    return false;
-  }
+    console.log('value', value);
+    if (!value) {
+        console.log('is null', value);
+        return false;
+    }
+    if (typeof value !== "string") {
+        console.log('is not string', value);
+        return false;
+    }
+    if (value.length > maxLength) {
+        console.log('is too long', value);
+        return false;
+    }
+    return true;
 }
