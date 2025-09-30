81b23351df73a7d3c34344145fdc175dc177943a
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 22:50:45 +0200

    contribution update

--- components/contribution.tsx
+++ components/contribution.tsx
@@ -4,6 +4,7 @@ import { useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { motion } from "framer-motion";
 import { contributionsData } from "@/lib/data";
+import { FaGithub } from "react-icons/fa";
 
 type ProjectProps = typeof contributionsData[number];
 
@@ -32,7 +33,7 @@ export default function Contribution({ title, description, type, url }: ProjectP
                 <a
                     className="mt-2 mb-2 bg-white p-4 flex h-6 text-gray-700 items-center gap-2 rounded-full focus:scale-[1.10] hover:scale-[1.10] active:scale-105 transition cursor-pointer borderBlack hover:text-gray-950 dark:bg-white/10 dark:text-white/60 dark:hover:text-gray-200"
                     href={url} target="_blank">
-                    <strong>{type}</strong> contribtution via Github
+                    <FaGithub/> GitHub <strong>{type}</strong> contribtution
                 </a>
             </div>
         </section>
