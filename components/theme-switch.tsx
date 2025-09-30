c5a7b4ab088d7fec5aa66fccf8ddef066db13f41
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 15:00:26 +0200

    theme local storage

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { BsSun } from 'react-icons/bs'
+'use client'
+
+import React, { useState } from 'react'
+import { BsMoon, BsSun } from 'react-icons/bs'
+
+type Theme = 'light' | 'dark';
 
 export default function ThemeSwitch() {
-  return (
-    <button className='fixed bottom-5 right-5'>
-        <BsSun />
-    </button>
-  )
+    const [theme, setTheme] = useState<Theme>("light");
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+            window.localStorage.setItem('theme', 'dark');
+        } else {
+            setTheme('light');
+            window.localStorage.setItem('theme', 'light');
+        }
+    }
+
+    return (
+        <button
+            onClick={toggleTheme}
+            className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all'>
+            {
+                theme === 'light' ? <BsSun /> : <BsMoon />
+            }
+        </button>
+    )
 }
