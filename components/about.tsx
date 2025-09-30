44ec7863880fba072555b983e8b750caf87a44b9
AdaLollA <lorenz.graaf@gmail.com>
2025-10-01 01:01:59 +0200

    about

--- components/about.tsx
+++ components/about.tsx
@@ -18,29 +18,35 @@ export default function About() {
             <SectionHeading>About me</SectionHeading>
             <p className="mb-3">
                 After graduating with a degree in{" "}
-                <span className="font-medium">Accounting</span>, I decided to pursue my
-                passion for programming. I enrolled in a coding bootcamp and learned{" "}
-                <span className="font-medium">full-stack web development</span>.{" "}
+                <span className="font-medium">Science</span>, I further persued my
+                passion for programming. I soon stared internships and learned{" "}
+                <span className="font-bold">full-stack web development</span>.{" "}
                 <span className="italic">My favorite part of programming</span> is the
                 problem-solving aspect. I <span className="underline">love</span> the
-                feeling of finally figuring out a solution to a problem. My core stack
-                is{" "}
-                <span className="font-medium">
-                    React, Next.js, Node.js, and MongoDB
+                feeling of finally figuring out a solution to a problem.
+            </p>
+
+            <p className="mb-3">
+                My core stacks are {" "}
+                <span className="font-bold">
+                    React / Next.js / Node.js / Vercel
+                </span> or {" "}
+                <span className="font-bold">
+                    Angular / Ionic / Node.js / Firebase
                 </span>
-                . I am also familiar with TypeScript and Prisma. I am always looking to
-                learn new technologies. I am currently looking for a{" "}
+                . I am also familiar with native development. I am always looking to
+                learn new technologies. Currently I am not looking for a{" "}
                 <span className="font-medium">full-time position</span> as a software
                 developer.
             </p>
 
             <p>
                 <span className="italic">When I'm not coding</span>, I enjoy playing
-                video games, watching movies, and playing with my dog. I also enjoy{" "}
-                <span className="font-medium">learning new things</span>. I am currently
+                video games, biking, and playing with my cat. I also enjoy{" "}
+                <span className="underline">learning new things</span>. I am currently
                 learning about{" "}
-                <span className="font-medium">history and philosophy</span>. I'm also
-                learning how to play the guitar.
+                <span className="italic">history and geopolitics</span>. I also
+                play the guitar, ukulele and a bit of piano.
             </p>
         </motion.section>
     )
