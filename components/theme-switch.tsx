d7efe0c4ed8d3c66a8e8110ced37e7d08b2db4ef
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 15:47:23 +0200

    opacity fixes rolled out

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -36,7 +36,7 @@ export default function ThemeSwitch() {
     return (
         <button
             onClick={toggleTheme}
-            className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all
+            className='fixed bottom-5 right-5 bg-white/70 w-[3rem] h-[3rem] backdrop-blur-[0.5rem] border border-white/40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all
             dark:bg-gray-950'>
             {
                 theme === 'light' ? <BsSun /> : <BsMoon />
