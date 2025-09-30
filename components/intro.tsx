004527a5ccc0d00f2e2d24a0abc1f1be86a88ffa
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 17:07:13 +0200

    content rework

--- components/intro.tsx
+++ components/intro.tsx
@@ -49,10 +49,9 @@ export default function Intro() {
                 animate={{ opacity: 1, y: 0 }}
                 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
                 <span className="font-bold">Hello, I'm Lorenz.</span> I'm a{" "}
-                <span className="font-bold">full-stack developer</span> with{" "}
-                <span className="font-bold">{new Date().getFullYear() - 2013} years</span> of experience. I enjoy
-                building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js)</span>.
+                <span className="italic">full-stack developer</span> with{" "}
+                <span className="font-bold">{new Date().getFullYear() - 2013} years</span> of experience. My focus is{" "}
+                <span className="underline">data science & seo</span>.
             </motion.h1>
 
             <motion.div
