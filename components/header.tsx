3e3e91b01c2c87d19c5dc479005fe5abcb2b1f56
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 22:43:11 +0200

    header resize fix

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSection();
     return (
         <header className="z-[999] relative">
-            <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white/40 bg-white/70 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
+            <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white/40 bg-white/70 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[42rem] sm:rounded-full
             dark:bg-gray-950/70 dark:border-black/40"
                 initial={{ y: -100, x: '-50%', opacity: 0 }}
                 animate={{ y: 0, x: '-50%', opacity: 1 }}>
