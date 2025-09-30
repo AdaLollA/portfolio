df9a9b11334d03b37dd1509c8690c98bbcd9c7b9
AdaLollA <lorenz.graaf@gmail.com>
2025-09-30 23:17:20 +0200

    project updates

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,12 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 import { IoRocketOutline } from "react-icons/io5";
+
+import ggdlImg from "@/public/ggdl.png";
+import avcImg from "@/public/avc.png";
+import anzImg from "@/public/ggdl.png";
+import websitesImg from "@/public/websites.png";
 
 export const links = [
   {
@@ -94,28 +96,28 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: ggdlImg,
   },
   {
     title: "AVC",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: avcImg,
   },
   {
     title: "Anz",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: anzImg,
   },
   {
     title: "Websites",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: websitesImg,
   }
 ] as const;
 
